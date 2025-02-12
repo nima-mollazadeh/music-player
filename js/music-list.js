cdc6f8c20c383593a79d2dae80461c35e0cfc9c9
XOCODE <143739049+nima-mollazadeh@users.noreply.github.com>
2025-02-12 14:56:34 +0330

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+  {
+    name: "Death Bed",
+    artist: "Powfu",
+    img: "Death_bed",
+    src: "Death_bed"
+  },
+  {
+    name: "Let me down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Gheseh Eshgh",
+    artist: "ebi",
+    img: "Gheseh Eshgh",
+    src: "Gheseh Eshgh"
+  }
+];
